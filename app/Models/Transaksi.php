e91740502463e8c99b24fbbb226b39c81df4278e
Ekky Sukmawati <ekkys99@gmail.com>
2024-01-07 13:32:39 +0700

    Add Relasi Tabel dengan eloquent Kategori & Transaksi

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+
+    protected $table = "transaksi";
+
+    protected $fillable = ['tanggal', 'jenis', 'kategori_id', 'nominal', 'keterangan'];
+
+    public function kategori()
+    {
+        return $this->belongsTo('App\Kategori');
+    }
+}
---
+++ database/migrations/2024_01_07_060520_create_transaksi_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tanggal');
+            $table->enum('jenis', ['Pemasukan', 'Pengeluaran']);
+            $table->integer('kategori_id');
+            $table->integer('nominal');
+            $table->text('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
